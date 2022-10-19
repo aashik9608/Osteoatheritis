75fe9d82c1d2efa4ba502255a4ef314be39a166f
Aashik Rasool <57311134+aashik9608@users.noreply.github.com>
2022-10-19 18:24:34 +0900

    Add files via upload

---
+++ imagesize.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import glob
+import os
+import cv2
+# open a Image
+image=Image.open("D:\\Gachon study\\Masters\\Masters\\1st sem\\Computer Vision\\Osteo\\train\\0\\Image_1.jpg")
+#display image
+image.show()
+# print(image)
+
+print(image.size)
